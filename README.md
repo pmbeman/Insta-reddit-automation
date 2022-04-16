cb2c0b65edd1233521b41a36ea9d0e6abdc551ff
Kinjal Kavish Boro <85004952+KeviTheDuck@users.noreply.github.com>
2022-04-16 12:20:34 +0530

    updates

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Insta-reddit-autmation
- A simple script to download posts from a particular subreddit and and then upload the media on instgram.
+# Insta-reddit-automation
+A simple script to download posts in the best available format using youtube-dl from a sub reddit and then upload that on instgram.
+
+    python main.py
+
+# Installation
+**1. Clone the repo**
+
+**2. Download all requirements**
+  
+    pip install -r requirements.txt
+
+
+
+# Configure the script
+**Get client ID and secret.**
+
+Visit [here](https://www.reddit.com/prefs/apps/) to create a reddit app and get the client ID and secret.
+
+Check [this](https://praw.readthedocs.io/en/stable/getting_started/quick_start.html) to learn more about it.
+
+Use this user-agent as an example: `android:com.example.myredditapp:v1.2.3 (by u/<reddit user-name>)`
+
+**Set the needed variables**
+* Open main.py and set the variables as required.
+  
+```python
+# Needed variables  
+ig_username = ""
+ig_password = ""
+reddit_client_id = ""
+reddit_client_secret = ""
+user_agent = ""  
+sub_reddit = ""
+```
+
+# Extras:
+* Delete the /config/ folder evertime you restart the script.
+* 
---
+++ main.py
@@ -0,0 +1,68 @@
+import praw
+import subprocess
+from instabot import Bot
+import time
+import os 
+
+# Needed variables
+ig_username = ""
+ig_password = ""
+reddit_client_id = ""
+reddit_client_secret = ""
+user_agent = ""  
+sub_reddit = ""
+
+# Instabot instance
+bot =  Bot()
+bot.login(username=ig_username,password=ig_password)
+
+# Reddit API 
+reddit = praw.Reddit(
+    client_id=reddit_client_id,
+    client_secret=reddit_client_secret,
+    user_agent=user_agent,
+    )
+
+subreddit = reddit.subreddit(sub_reddit)
+a = 0
+while True:
+    f = open("tags.txt", "r")
+    tags = f.read()
+    f.close
+    
+    # Get URL and title of the subreddit
+    for submission in subreddit.new(limit=1):
+        post_url = str(submission.url)
+        title = submission.title
+
+        # Extension of the URL
+        extension = ""
+        if post_url.endswith(".jpg"):
+            extension = ".jpg"
+        elif post_url.endswith(".gif"):
+            extension = ".gif"
+        elif post_url.endswith(".mp4"):
+            extension = ".mp4"
+        elif post_url.endswith(".png"):
+            extension == ".png"
+        else:
+            print("Extension not set")
+            continue
+        
+        media_name = 'image' + str(a) + extension
+        caption = title + "\n\n\n" + tags
+
+        # Download the media from the URL
+        subprocess.run('youtube-dl '+ post_url + ' --output "posts/'+ media_name + '"' , shell=True, check=True, capture_output=True)
+        
+        # media upload
+        if extension == ".jpg" or extension == ".png":
+            bot.upload_photo(photo="posts/" + media_name, caption=caption)
+        elif extension == ".mp4" or extension == ".gif":
+            bot.upload_video(video="posts/" + media_name, caption=caption)
+        else:
+            continue
+        os.remove("posts/" + media_name )
+        time.sleep()
+    a = a+1
+    
